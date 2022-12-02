1e971db6e8c603dd21563b64ca5dca0ef95c1a0e
Mathias Faulkner <matf@netcompany.com>
2022-12-02 11:51:35 +0100

    misc to 1

--- puzzles/1/main.js
+++ puzzles/1/main.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
-var input = fs.readFileSync("./1/input_1.txt", "utf-8");
+var input = fs.readFileSync("./puzzles/1/input.txt", "utf-8");
 var inputByElf = input.split("\n\r");
 
 var listOfElfCaloriesSum = [];
@@ -23,10 +23,10 @@ console.log("1: Elf #" + (listOfElfCaloriesSum.indexOf(maxElf1)+1) + " calories:
 
 listAltered.splice(listAltered.indexOf(maxElf1), 1);
 var maxElf2 = listAltered.max();
-console.log("2: Elf #" + (listOfElfCaloriesSum.indexOf(maxElf2)+1) + "   calories: " + maxElf2);
+console.log("2: Elf #" + (listOfElfCaloriesSum.indexOf(maxElf2)+1) + " calories: " + maxElf2);
 
 listAltered.splice(listAltered.indexOf(maxElf2), 1);
 var maxElf3 = listAltered.max();
-console.log("3: Elf #" + (listOfElfCaloriesSum.indexOf(maxElf3)+1) + "   calories: " + maxElf3);
+console.log("3: Elf #" + (listOfElfCaloriesSum.indexOf(maxElf3)+1) + " calories: " + maxElf3);
 
 console.log("Total: " + (maxElf1 + maxElf2 + maxElf3));
