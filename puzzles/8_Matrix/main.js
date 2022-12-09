edf821d0e18e6bdaff6e9e6ef87faacc7f26eb37
Mathias Faulkner <matf@netcompany.com>
2022-12-09 13:30:27 +0100

    wrongly edited 8

--- puzzles/8_Matrix/main.js
+++ puzzles/8_Matrix/main.js
@@ -14,20 +14,78 @@ inputSplit.forEach(function (line, i) {
 })
 
 const identityMatrix = math.matrix([]);
-function MoveHead(value, index, treeBesidesIndex) {
-  var treeBesides = matrix.get(treeBesidesIndex);
-  if (treeBesides < value) {
+function setIdentityMatrix(value, index, treeBesidesIndex) {
+  try {
+    var treeBesides = matrix.get(treeBesidesIndex);
+    if (treeBesides < value) {
+      identityMatrix.set(index, 1);
+    } else {
+      identityMatrix.set(index, 0);
+      return false;
+    }
+  } catch (error) {
     identityMatrix.set(index, 1);
-  } else {
-    identityMatrix.set(index, 0);
   }
+  return true;
 }
 
+var matrixSize = matrix.size();//.map(i => i-1);
+matrix.forEach(function (value, index) {
+  var visible = false;
+  //right
+  if (!visible) {
+    for (var y = index[1] + 1; y <= matrixSize[1]; y++) {
+      var treeBesidesIndex = [index[0], y];
+      visible = setIdentityMatrix(value, index, treeBesidesIndex);
+      if (!visible) {
+        break;
+      }
+    }
+  }
+  //left
+  if (!visible) {
+    for (var y = index[1] - 1; y >= -1; y--) {
+      var treeBesidesIndex = [index[0], y];
+      visible = setIdentityMatrix(value, index, treeBesidesIndex);
+      if (!visible) {
+        break;
+      }
+    }
+  }
+  //down
+  if (!visible) {
+    for (var x = index[0] + 1; x <= matrixSize[0]; x++) {
+      var treeBesidesIndex = [x, index[1]];
+      visible = setIdentityMatrix(value, index, treeBesidesIndex);
+      if (!visible) {
+        break;
+      }
+    }
+  }
+  //up
+  if (!visible) {
+    for (var x = index[0] - 1; x >= -1; x--) {
+      var treeBesidesIndex = [x, index[1]];
+      visible = setIdentityMatrix(value, index, treeBesidesIndex);
+      if (!visible) {
+        break;
+      }
+    }
+  }
+})
+
+var partOneSum = 0;
+identityMatrix.forEach(function (value) {
+  partOneSum += value;
+})
+
+console.log("Part One Sum: " + partOneSum);
+
+////////////////////////////// PART TWO /////////////////////////////////
 const scenicSumMatrix = math.matrix([]);
 var matrixSize = matrix.size().map(i => i-1);
 var score;
-inputSplit.forEach(function (line, i) {
-  var line = line.replace("\r", "");
+matrix.forEach(function (value, index) {
   var visible = false;
   score = 1;
   //right
