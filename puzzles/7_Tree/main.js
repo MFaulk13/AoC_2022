27720eeb4bac1a63c941ef5d8cbde5c8e71c9106
Mathias Faulkner <matf@netcompany.com>
2022-12-09 10:13:48 +0100

    fixed 7

--- puzzles/7_Tree/main.js
+++ puzzles/7_Tree/main.js
@@ -47,6 +47,10 @@ class Tree {
       }
       return false;
     }
+
+    insertToParentNode(parentNode, key, value = key) {
+      parentNode.children.push(new TreeNode(key, value, parentNode));
+    }
   
     remove(key) {
       for (let node of this.preOrderTraversal()) {
@@ -70,8 +74,11 @@ class Tree {
 
 
 
+
+
+
 var fs = require("fs");
-var input = fs.readFileSync("./puzzles/7/input_test.txt", "utf-8");
+var input = fs.readFileSync("./puzzles/7_Tree/input.txt", "utf-8");
 var inputSplit = input.split("\n");
 
 function getLast(line) {
@@ -83,43 +90,46 @@ function hasNumber(myString) {
 }
 
 const tree = new Tree("/");
-var currentDir;
+let currentDir;
 inputSplit.forEach(function(line) {
     var line = line.replace("\r", "");
 
     // Generate tree
     if (line.includes("$ cd")) {
         if (line.includes("/")) {
-            currentDir = "/";
+            currentDir = tree.root;
         }
         else if (getLast(line) == "..") {
             // go one out
-            if (currentDir != "/") {
-                currentDir = tree.find(currentDir).parent.key;
+            if (currentDir.key != "/") {
+                currentDir = currentDir.parent;
             }
         }
-        else currentDir = getLast(line);
+        else {
+          currentDir = currentDir.children
+                        .filter(function(o) { return o.key === getLast(line); })
+                        [0];
+        }
     }
     if (line.includes("$ ls")) {
         // do nothing
     }
     if (line.includes("dir")) {
-        tree.insert(currentDir, getLast(line));
+        tree.insertToParentNode(currentDir, getLast(line));
     }
     if (hasNumber(line)) {
         var file = line.split(" ");
-        tree.insert(currentDir, file[1], Number(file[0]));
+        tree.insertToParentNode(currentDir, file[1], Number(file[0]));
     }
 });
 
 function getAllDirs() {
     return [...tree.preOrderTraversal()]
-            .map(x => x.value)
-            .filter(function(o) { return typeof(o) === "string"; });
+            .filter(function(o) { return typeof(o.value) === "string"; });
 }
 
 function getSumOfDir(dir) {
-    return [...tree.preOrderTraversal(tree.find(dir))]
+    return [...tree.preOrderTraversal(dir)]
             .map(x => x.value)
             .filter(function(o) { return typeof(o) === "number"; })
             .reduce((a, b) => a + b, 0);
@@ -129,9 +139,31 @@ var allDirs = getAllDirs();
 var partOneSum = 0;
 allDirs.forEach(function(dir) {
     var sum = getSumOfDir(dir);
-    console.log("Dir: " + dir + " Sum: " + sum);
+    console.log("Dir: " + dir.key + " Sum: " + sum);
     if (sum <= 100000) {
         partOneSum = partOneSum + sum;
     }
 });
 console.log("Part One Sum: " + partOneSum);
+
+//////////// PART TWO ////////////////
+console.log("\nPart Two");
+var totalDiskSpace = 70000000;
+console.log("Disk space available: " + totalDiskSpace);
+var totalUsedSpace = getSumOfDir(tree.root);
+console.log("Total used space: " + totalUsedSpace);
+var totalUnusedSpace = totalDiskSpace - totalUsedSpace;
+console.log("Total unused space: " + totalUnusedSpace);
+var neededUnusedSpace = 30000000;
+console.log("Needed unused space: " + neededUnusedSpace);
+var totalMinimumSpaceDelete = neededUnusedSpace - totalUnusedSpace;
+console.log("Minimum space needed to delete: " + totalMinimumSpaceDelete);
+
+var smallestSizeNeeded = totalDiskSpace;
+allDirs.forEach(function(dir) {
+  var sum = getSumOfDir(dir);
+  if (sum >= totalMinimumSpaceDelete && sum < smallestSizeNeeded) {
+    smallestSizeNeeded = sum;
+  }
+});
+console.log("Part Two - Smallest Dir to delete has size: " + smallestSizeNeeded);
